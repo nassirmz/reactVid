2b7dbaf990ff3ebca2462c57c4b0c789a7e235d4
nassir <nassirmz@gmail.com>
2016-09-09 18:54:21 -0700

    Import Searchbar

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from 'search_bar';
+
 const API_KEY = 'AIzaSyChawuvZnh4Ts-eY0P-q_rJ2_vmwAYHeO8';
+
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
