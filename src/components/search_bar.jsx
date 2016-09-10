b0880cab2802651608ddaad15fc49164e9a06429
nassir <nassirmz@gmail.com>
2016-09-09 19:46:36 -0700

    Set state of SearchBar

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -1,15 +1,22 @@
 import React, { Component} from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
   render() {
     return (
-      <input onChange={this.handleInputChange}/>
+      <div>
+        <input onChange={this.handleInputChange.bind(this)}/>
+        Value of the input: {this.state.term}
+      </div>
     )
   }
-
   //event handler for input changes
   handleInputChange(e) {
-    console.log(e.target.value);
+    this.setState({ term: e.target.value });
   }
 }
 
