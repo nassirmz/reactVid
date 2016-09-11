df4c0631903480ea1726cdcc187ad0a51af7b813
nassir <nassirmz@gmail.com>
2016-09-10 19:39:33 -0700

    Fetch youtube videos

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from 'search_bar';
 
 const API_KEY = 'AIzaSyChawuvZnh4Ts-eY0P-q_rJ2_vmwAYHeO8';
+
+YTSearch({key: API_KEY, term: 'yusuf'}, function (data) {
+  console.log(data);
+});
 
 const App = () => {
   return (
